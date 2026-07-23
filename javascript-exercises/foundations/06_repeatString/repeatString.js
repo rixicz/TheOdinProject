1c963a4298216d4999d2e01fd9b6906f3c8c9407
rixicz <patrik.smidoch@hotmail.com>
2026-07-23 09:50:54 +0200

    repeatString done

--- javascript-exercises/foundations/06_repeatString/repeatString.js
+++ javascript-exercises/foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    if (times < 0) {
+        return "ERROR"
+    }
+    let repeatedWords = "";
+    for (i = 0; i < times; i++) {
+        repeatedWords += word
+    }
+    return repeatedWords
 };
 
 // Do not edit below this line

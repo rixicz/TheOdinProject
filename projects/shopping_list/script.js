6e6df00d57bde26df54dd834a00b3fce31c4404a
rixicz <patrik.smidoch@hotmail.com>
2026-07-26 21:02:55 +0200

    done the shopping_list

--- projects/shopping_list/script.js
+++ projects/shopping_list/script.js
@@ -1,10 +1,24 @@
 const btn = document.querySelector("button");
 const unList = document.querySelector("ul")
-btn.addEventListener("click", (event) => {
-    let userInput = document.getElementById("item").value
-    let grocery = document.createElement("li")
-    grocery.textContent = userInput
-    unList.appendChild(grocery)
-    event.preventDefault();
-    document.querySelector("input").reset()
+
+
+btn.addEventListener("click", () => {
+    let userInput = document.getElementById("item");
+    let grocery = document.createElement("li");
+    let span = document.createElement("span");
+    let deleteBtn = document.createElement("button");
+    
+    span.textContent = userInput.value
+    deleteBtn.textContent = "Delete"
+
+    deleteBtn.addEventListener("click", function () { // important to add the event listener in the function where the button is created
+        this.parentNode.remove() // removes the parent element of the button - li
+    })
+    
+    grocery.appendChild(span);
+    grocery.appendChild(deleteBtn);
+    
+    unList.appendChild(grocery);
+    userInput.value = "";
+    userInput.focus() // no need to click again onto the input box
 });

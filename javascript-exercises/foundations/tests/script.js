cc3ab23503d748ed3ddd99898ab68731fed0d96f
rixicz <patrik.smidoch@hotmail.com>
2026-08-01 22:54:38 +0200

    starting out with another js-exercise

---
+++ javascript-exercises/foundations/tests/script.js
@@ -0,0 +1,25 @@
+let users = [
+  {id: 'john', name: "John Smith", age: 20},
+  {id: 'ann', name: "Ann Smith", age: 24},
+  {id: 'pete', name: "Pete Peterson", age: 31},
+];
+
+function groupById(array) {
+  let userObj = {}
+  array.forEach((user) => {
+    userObj[user.id] = user
+  })
+  return userObj
+}
+let usersById = groupById(users);
+alert(usersById)
+
+/*
+// after the call we should have:
+
+usersById = {
+  john: {id: 'john', name: "John Smith", age: 20},
+  ann: {id: 'ann', name: "Ann Smith", age: 24},
+  pete: {id: 'pete', name: "Pete Peterson", age: 31},
+}
+*/
